bb43adc8e82ea81751bef100c57756fe0e7770b2
gabrielluizsf <gabrielluizads123@gmail.com>
2024-05-08 10:29:36 -0300

    Add GenericJSON type for flexible JSON object representation

--- json_test.go
+++ json_test.go
@@ -34,3 +34,37 @@ func TestJSON(t *testing.T) {
 	}
 	validateValues()
 }
+
+func TestGenericJSON(t *testing.T) {
+	type Developer struct {
+		Name string `json:"name"`
+		Job  string `json:"job"`
+	}
+	json := GenericJSON[[]Developer]{
+		"devs": []Developer{
+			{Name: "Gabriel Luiz", Job: "Backend Developer"},
+		},
+	}
+	buf, err := json.Marshal()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var data struct {
+		Devs []Developer `json:"devs"`
+	}
+	if err := DecodeJSON(bytes.NewBuffer(buf), &data); err != nil {
+		t.Fatal(err)
+	}
+	validateValues := func() {
+		if data.Devs[0].Name != json["devs"][0].Name {
+			t.Fatal("Not Decoded")
+		}
+	}
+	validateValues()
+	name := "gabrielluizsf"
+	json["devs"][0].Name = name
+	if err := json.Unmarshal(&data); err != nil {
+		t.Fatal(err)
+	}
+	validateValues()
+}
